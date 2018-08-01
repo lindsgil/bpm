a4782e7210d08abc1211109112db5a54db558f3f
Lindsay Gilbert <Lindsaygilbert722@gmail.com>
2018-07-31 22:43:35 -0700

    uupated bpm

--- app.js
+++ app.js
@@ -1,15 +1,54 @@
+var CONTESTANT_ONE_BPM = 114;
+var CONTESTANT_TWO_BPM = 97;
+var CONTESTANT_THREE_BPM = 95;
+
+function startTimer() {
+var presentTime = document.getElementById('blink-two').innerHTML;
+var timeArray = presentTime.split(/[:]+/);
+var m = timeArray[0];
+var s = checkSecond((timeArray[1] - 1));
+if(s==59){m=m-1}
+if (m<0) {
+  return document.getElementById('blink-two').innerHTML = "0:00";
+}
+document.getElementById('blink-two').innerHTML =
+m + ":" + s;
+setTimeout(startTimer, 1000);
+}
+
+function checkSecond(sec) {
+if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
+if (sec < 0) {sec = "59"};
+return sec;
+}
+
 $(document).ready(function() {
     var f = document.getElementById('blink-one');
     var g = document.getElementById('blink-two');
     var h = document.getElementById('blink-three');
-    setInterval(function() {
-        f.style.color = (f.style.color == 'white' ? '' : 'white');
-	g.style.color = (g.style.color == 'white' ? '' : 'white');
-  h.style.color = (h.style.color == 'white' ? '' : 'white');
-    }, 500);
+
+    var i = document.getElementById('header-one');
+    var j = document.getElementById('header-two');
+    var k = document.getElementById('header-three');
+
+    var bpmOne = document.getElementById('bpm-one');
+    var bpmTwo = document.getElementById('bpm-two');
+    var bpmThree = document.getElementById('bpm-three');
+
+    f.innerText = CONTESTANT_ONE_BPM;
+    g.innerText = CONTESTANT_TWO_BPM;
+    h.innerText = CONTESTANT_THREE_BPM;
+
+
 
 });
 
+    var bpmOne = document.getElementById('bpm-one');
+    var bpmTwo = document.getElementById('bpm-two');
+    var bpmThree = document.getElementById('bpm-three');
+    var i = document.getElementById('header-one');
+    var j = document.getElementById('header-two');
+    var k = document.getElementById('header-three');
 
     var p = document.getElementById('blink-one');
     var q = document.getElementById('blink-two');
@@ -20,36 +59,92 @@ $(document).ready(function() {
       let numberTwo = parseInt(q.innerText);
       let numberThree = parseInt(w.innerText);
       console.log("event.key", event.key);
+      console.log(number);
       switch (event.key) {
         case "Enter":
           q.style.visibility = "hidden";
           p.style.visibility = "hidden";
           w.style.visibility = "hidden";
         break;
-        case "q":
+        case "u":
           number++;
           p.innerText = number;
           break;
-        case "a":
+        case "j":
           number--;
-          p.innerText = number;
-          break;
-        case "t":
+          if (number >= 0) {
+            p.innerText = number;
+            break;
+          } else {
+            p.innerText = 0;
+            break;
+          }
+        case "i":
           numberTwo++;
           q.innerText = numberTwo;
           break;
-        case "g":
+        case "k":
           numberTwo--;
-          q.innerText = numberTwo;
-          break;
-        case "ArrowUp":
+          if (numberTwo >= 0) {
+            q.innerText = numberTwo;
+            break;
+          } else {
+            q.innerText = 0;
+            break;
+          }
+        case "o":
           numberThree++;
           w.innerText = numberThree;
           break;
-        case "ArrowDown":
+        case "l":
           numberThree--;
-          w.innerText = numberThree;
-          break;
+          if (numberThree >= 0) {
+            w.innerText = numberThree;
+            break;
+          } else {
+            w.innerText = 0;
+            break;
+          }
+        case "1":
+        p.innerText = ":)";
+        q.style.visibility = "hidden";
+        w.style.visibility = "hidden";
+        bpmOne.style.visibility = "hidden";
+        bpmTwo.style.visibility = "hidden";
+        bpmThree.style.visibility = "hidden";
+        break;
+        case "2":
+        q.innerText = ":)";
+        p.style.visibility = "hidden";
+        w.style.visibility = "hidden";
+        bpmOne.style.visibility = "hidden";
+        bpmTwo.style.visibility = "hidden";
+        bpmThree.style.visibility = "hidden";
+        break;
+        case "3":
+        w.innerText = ":)";
+        q.style.visibility = "hidden";
+        p.style.visibility = "hidden";
+        bpmOne.style.visibility = "hidden";
+        bpmTwo.style.visibility = "hidden";
+        bpmThree.style.visibility = "hidden";
+        break;
+        case "5":
+        i.style.visibility = "hidden";
+        j.style.visibility = "hidden";
+        k.style.visibility = "hidden";
+        p.style.visibility = "hidden";
+        w.style.visibility = "hidden";
+        bpmOne.style.visibility = "hidden";
+        bpmTwo.style.visibility = "hidden";
+        bpmThree.style.visibility = "hidden";
+        q.style.marginLeft = "110px";
+        q.style.marginTop = "170px";
+        q.style.fontSize = "180px";
+        q.innerText = 05 + ":" + 00;
+        startTimer();
+        break;
+
         default:
           return;
       }
